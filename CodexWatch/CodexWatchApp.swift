66b531a4bd50382e59373b6c1e71fa78fbad8655
Dionysis <daino92@gmail.com>
2026-09-18 10:54:38 +0300

    Initial Commit

---
+++ CodexWatch/CodexWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodexWatchApp.swift
+//  CodexWatch
+//
+//  Created by Dionysis Kalepanagos on 18/9/26.
+//
+
+import SwiftUI
+
+@main
+struct CodexWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
